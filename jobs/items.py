8b2511d78a520cb7706ec995cc4cb3bb2ea76a5d
vickey <1143604452@qq.com>
2018-02-08 23:54:16 +0800

    scrapy scratch jobs info at zhipin.com

--- jobs/items.py
+++ jobs/items.py
@@ -11,13 +11,13 @@ import scrapy
 class JobsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    # job_title = scrapy.Field()
+    job_title = scrapy.Field()
     job_salary = scrapy.Field()
     job_requirement = scrapy.Field()
     job_addr = scrapy.Field()
     job_exp = scrapy.Field()
     job_edu = scrapy.Field()
-    # job_tags = scrapy.Field()
+    job_tags = scrapy.Field()
     company_name = scrapy.Field()
     company_employee_num = scrapy.Field()
     company_type = scrapy.Field()
